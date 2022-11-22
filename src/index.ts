9b643d429f04a402c7f6b62e09db0ddebe268057
Marco <marcokammer59@gmail.com>
2022-11-22 09:49:14 +0200

    index.ts export fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './quaternion'
-export * from './matrix'
-export * from './vec'
+export * as quaternion from './quaternion'
+export * as mat from './matrix'
+export * as vec3 from './vec'
+export * as vec2 from './vec2'
