7d0af3d0bda6c123c35d2db629e7058f88cb3cd4
Marco <marcokammer59@gmail.com>
2022-12-30 16:47:16 +0200

    cleanup n added comments to vec4 functions

--- src/vec3.ts
+++ src/vec3.ts
@@ -89,12 +89,12 @@ export const clampMagnitude = (v: vec3, maxLength: number): vec3 => {
 //Returns this vector with a magnitude of 1
 export const normalized = (v: vec3): vec3 => magnitude(v) > mathf.kEpsilon ? create(v[0] / magnitude(v), v[1] / magnitude(v), v[2] / magnitude(v)) : zero
 //Makes this vector have a magnitude of 1.
-export const normalize = (v: vec3): void => {
+export const Normalize = (v: vec3): vec3 => {
     const mag = magnitude(v)
-    if (mag > 0.00001) {
-        { v[0] / mag, v[1] / mag, v[2] / mag }
+    if (mag > mathf.kEpsilon) {
+        return create(v[0] / mag, v[1] / mag, v[2] / mag)
     } else {
-        zero
+        return zero
     }
 }
 type Negate = (v: vec3) => vec3
@@ -103,8 +103,9 @@ export const negate: Negate = (v) => create(-v[0], -v[1], -v[2])
 export const scalarProjection = (v1: vec3, v2: vec3): number => dot(normalized(v1), v2)
 export const VectorProjection = (v1: vec3, v2: vec3): vec3 => {
     // would get normalize projection point?
-    const v1Norm = normalized(v1)
+    const v1Norm = Normalize(v1)
     const scProj = scalarProjection(v1, v2)
+    scalarMultiplication(v1Norm, scProj)
     return create(v1Norm[0] * scProj, v1Norm[1] * scProj, v1Norm[2] * scProj)
 }
 //Returns the distance between a and b.
@@ -113,7 +114,7 @@ export const distance: Distance = (v1, v2) => Math.sqrt(distanceSqrt(v1, v2))
 // export const distance = (v1: vec3, v2: vec3): number => Math.sqrt(distanceSqrt(v1, v2))
 export const distanceSqrt = (v1: vec3, v2: vec3): number => (v2[0] - v1[0]) * (v2[0] - v1[0]) + (v2[1] - v1[1]) * (v2[1] - v1[1]) + (v2[2] - v1[2]) * (v2[2] - v1[2])
 //Linearly interpolates between two points.
-export const Lerp = (a: vec3, b: vec3, t: number): vec3 => create(mathf.Lerp(a[0], b[0], t), mathf.Lerp(a[1], b[1], t), mathf.Lerp(a[2], b[2], t))
+export const Lerp = (a: vec3, b: vec3, t: number): vec3 => create(mathf.LerpClamped(a[0], b[0], t), mathf.LerpClamped(a[1], b[1], t), mathf.LerpClamped(a[2], b[2], t))
 export const Lerps = (a: vec3, b: vec3, t: vec3): vec3 => create(mathf.Lerp(a[0], b[0], t[0]), mathf.Lerp(a[1], b[1], t[1]), mathf.Lerp(a[2], b[2], t[2]))
 //Linearly interpolates between two vectors.
 export const LerpUnclamped = (a: vec3, b: vec3, t: number): vec3 => create(a[0] + (b[0] - a[0]) * t, a[1] + (b[1] - a[1]) * t, a[2] + (b[2] - a[2]) * t)
--- test/vec4.test.ts
+++ test/vec4.test.ts
@@ -17,14 +17,14 @@ describe('vec4', () => {
         expect(v[3]).toBe(4)
     })
     test('one static vector', () => {
-        const v = vec4.one()
+        const v = vec4.one
         expect(v[0]).toBe(1)
         expect(v[1]).toBe(1)
         expect(v[2]).toBe(1)
         expect(v[3]).toBe(1)
     })
     test('zero static vector', () => {
-        const v = vec4.zero()
+        const v = vec4.zero
         expect(v[0]).toBe(0)
         expect(v[1]).toBe(0)
         expect(v[2]).toBe(0)
@@ -33,12 +33,12 @@ describe('vec4', () => {
     test('Distance, v1(2,3,0,2) and v2(5,7,0,2) values given', () => {
         const v1 = vec4.create(2, 3, 0, 2)
         const v2 = vec4.create(5, 7, 0, 2)
-        const dist = vec4.distance(v1, v2)
+        const dist = vec4.Distance(v1, v2)
         expect(dist).toBe(5)
     })
     test('Normalized, v(3,1,2,4) values given', () => {
         const v = vec4.create(3, 1, 2, 4)
-        const norm = vec4.Normalized(v)
+        const norm = vec4.normalized(v)
         expect(parseFloat(norm[0].toFixed(3))).toBe(0.548)
         expect(parseFloat(norm[1].toFixed(3))).toBe(0.183)
         expect(parseFloat(norm[2].toFixed(3))).toBe(0.365)
@@ -46,11 +46,11 @@ describe('vec4', () => {
     })
     test('Normalize, v(3,1,2,4) values given', () => {
         const v = vec4.create(3, 1, 2, 4)
-        const news = vec4.create(0, 0, 0, 0)
-        const norm = vec4.normalize(news, v)
+        const norm = vec4.Normalize(v)
         expect(parseFloat(norm[0].toFixed(3))).toBe(0.548)
         expect(parseFloat(norm[1].toFixed(3))).toBe(0.183)
         expect(parseFloat(norm[2].toFixed(3))).toBe(0.365)
         expect(parseFloat(norm[3].toFixed(3))).toBe(0.730)
     })
 })
+
